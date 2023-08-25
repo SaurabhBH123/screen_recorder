da0fd1c08ed2c839602fa5bccac32d1bea354489
Saurabh Bhandari <bhandarisaurabh143@gmail.com>
2023-08-25 10:41:42 +0530

    removed node_modules folder

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,24 @@
 const express = require("express")
 const cors = require("cors")
 const mongoose = require("mongoose")
-
+const { router } = require("./routes/user.routes")
+require("dotenv").config()
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 
+app.use("/",router)
 app.get("/",(req,res)=>{
     res.send("Hello Everyone!")
 })
 
-app.listen(8000,async()=>{
+app.listen(process.env.port,async()=>{
     try {
-        await mongoose.connect()
+        await mongoose.connect(process.env.mongoURL)
         console.log("connected to DB")
     } catch (error) {
         console.log(error);
     }
-    console.log(`Server is running at port 8000`);
+    console.log(`Server is running at port ${process.env.port}`);
 })
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,12 +1,33 @@
 const express = require("express")
+const { User } = require("../models/user.model")
 const router = express.Router()
+const jwt = require("jsonwebtoken")
 
 router.post("/register",async(req,res)=>{
     const {name,email} = req.body
 
     try {
-        
+        const user = new User({name,email})
+        await user.save()
+        res.status(200).send({"msg":"New User Registered!"})
     } catch (error) {
-        
+        res.status(400).send({"msg":error.message})
     }
 })
+
+router.post("/login",async(req,res)=>{
+    const {name,email} = req.body;
+
+    try {
+        const user = await User.findOne({email})
+        if(user){
+            res.status(200).send({"msg":"Login Successful!","token":jwt.sign({email},"saurabh@123")})
+        }else{
+            res.status(400).send({"msg":"No user found!"})
+        }
+    } catch (error) {
+        res.status(400).send({"msg":error.message})
+    }
+})
+
+module.exports = {router}
